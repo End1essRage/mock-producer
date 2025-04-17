0aead40968bf55771b22ce220bb622415ae42b85
Nazar <end1essrage@mail.ru>
2025-04-17 11:00:54 +0300

    добавил флаги генерации и шаблоны

--- generator/flags.go
+++ generator/flags.go
@@ -7,12 +7,30 @@ import (
 func (g *Generator) RegisterFlags() {
 	gen := make(Gen)
 
+	gen["randUuid"] = func() any {
+		return gofakeit.UUID()
+	}
+
 	gen["randString"] = func() any {
 		return gofakeit.Word()
 	}
 
 	gen["randNum"] = func() any {
 		return gofakeit.Int()
+	}
+
+	gen["randBool"] = func() any {
+		return gofakeit.Bool()
+	}
+
+	// будущая дата
+	gen["randFDate"] = func() any {
+		return gofakeit.FutureDate()
+	}
+
+	// прошедшая дата
+	gen["randPDate"] = func() any {
+		return gofakeit.PastDate()
 	}
 
 	g.flags = gen
